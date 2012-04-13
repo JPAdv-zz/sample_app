ce31862e24a44a24f1d06e324379a21d391b761b
Jose Padilla <jose.padilla.88@gmail.com>
2012-04-13 16:58:34 -0700

    Added new tests.

--- spec/requests/static_pagegs_spec.rb
+++ spec/requests/static_pagegs_spec.rb
@@ -6,17 +6,23 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Sample App'" do
-      visit '/static_pagegs/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
+    	it "should have the h1 'Sample App'" do
+      		visit '/static_pagegs/home'
+      		page.should have_selector('h1', :text => 'Sample App')
+    	end
 
-    it "should have the title 'Home'" do
-    	visit '/static_pagegs/home'
-    	page.should have_selector('title',
+    	it "should have the base title" do
+      		visit '/static_pagegs/home'
+      		page.should have_selector('title',
+                        :text => "#{base_title}")
+    	end
+
+    	it "should have a costum page title" do
+    		visit '/static_pagegs/home'
+    		page.should_not have_selector('title',
     						:text => "#{base_title} | Home")
-    end
-  end
+   		end
+ 	end
 
 	describe "Help page" do
 
