baa2fa94510a9eec7c6576326736071b5292375b
Jose Padilla <jose.padilla.88@gmail.com>
2012-05-12 23:27:50 -0700

    Finished Chapter 7 Exercises

--- spec/requests/user_pagegs_spec.rb
+++ spec/requests/user_pagegs_spec.rb
@@ -31,6 +31,13 @@ describe "User pages" do
       end
     end
 
+    describe "error messages" do
+      before { click_button submit }
+
+      it { should have_selector('title', text: 'Sign Up') }
+      it { should have_content('error') }
+    end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Jason Armata"
@@ -39,6 +46,13 @@ describe "User pages" do
         fill_in "Confirmation", with: "foobar"
       end
 
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('armata@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end  
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
